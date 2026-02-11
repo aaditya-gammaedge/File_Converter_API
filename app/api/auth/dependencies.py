c2596f6c0fbefba8860a4f9ee8e86a678f9196d8
aaditya.jaiswal <aaditya.jaiswal@gammaedge.io>
2026-02-11 11:08:27 +0530

    updated dependencies

--- app/api/auth/dependencies.py
+++ app/api/auth/dependencies.py
@@ -1,13 +1,54 @@
+# from fastapi import Depends, HTTPException, status
+# from fastapi.security import OAuth2PasswordBearer,HTTPBearer
+# from sqlalchemy.ext.asyncio import AsyncSession
+# from uuid import UUID
+# from app.api.auth.jwt import decode_access_token
+# from app.db.db import AsyncSessionLocal
+# from app.db.models import User
+
+
+# oauth2_scheme = HTTPBearer(tokenUrl="/auth/login")
+# bearer_scheme = HTTPBearer(auto_error=True)
+
+# async def get_db() -> AsyncSession:
+#     async with AsyncSessionLocal() as session:
+#         yield session
+
+
+# async def get_current_user(
+#     token: str = Depends(oauth2_scheme),
+#     db: AsyncSession = Depends(get_db)
+# ) -> User:
+#     user_id: UUID | None = decode_access_token(token)
+
+#     if not user_id:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Invalid or expired token"
+#         )
+
+#     user = await db.get(User, user_id)
+
+#     if not user:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="User not found"
+#         )
+
+#     return user
+
+
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 from uuid import UUID
+
 from app.api.auth.jwt import decode_access_token
 from app.db.db import AsyncSessionLocal
 from app.db.models import User
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+bearer_scheme = HTTPBearer(auto_error=True)
 
 
 async def get_db() -> AsyncSession:
@@ -16,9 +57,11 @@ async def get_db() -> AsyncSession:
 
 
 async def get_current_user(
-    token: str = Depends(oauth2_scheme),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
     db: AsyncSession = Depends(get_db)
 ) -> User:
+    token = credentials.credentials  # <-- actual JWT string
+
     user_id: UUID | None = decode_access_token(token)
 
     if not user_id:
