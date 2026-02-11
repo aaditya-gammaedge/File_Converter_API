b3372f56c8894e588ee94df8e7a3f06c0673271e
aaditya.jaiswal <aaditya.jaiswal@gammaedge.io>
2026-02-11 11:42:37 +0530

    updated:config.py

--- app/config.py
+++ app/config.py
@@ -1,16 +1,44 @@
-import os 
+# import os 
+# from dotenv import load_dotenv
+
+# from pathlib import Path
+
+
+# load_dotenv(".env")   
+
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+# JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
+# JWT_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES"))
+# SUPABASE_URL = os.getenv("SUPABASE_URL")
+# SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+
+import os
 from dotenv import load_dotenv
 
-from pathlib import Path
+# Load .env file
+load_dotenv(".env")
 
-
-load_dotenv(".env")   
-
-
+# Database
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# JWT
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
-JWT_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES"))
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+JWT_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES", "60"))
 
+# # Supabase
+# SUPABASE_URL = os.getenv("SUPABASE_URL")
+# SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
+# Optional defaults
+MAX_UPLOAD_SIZE_MB = int(os.getenv("MAX_UPLOAD_SIZE_MB", "20"))
+PRESIGNED_URL_EXPIRE_SEC = int(os.getenv("PRESIGNED_URL_EXPIRE_SEC", "300"))
+SUPABASE_S3_ENDPOINT = os.getenv("SUPABASE_S3_ENDPOINT")
+    
+SUPABASE_ACCESS_KEY = os.getenv("SUPABASE_ACCESS_KEY")
+    
+SUPABASE_SECRET_KEY =os.getenv("SUPABASE_SECRET_KEY")
+SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET")
