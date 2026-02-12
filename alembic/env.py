18d0a18bc5d0a928e5e7c46f084ce96b17d2e93e
aaditya.jaiswal <aaditya.jaiswal@gammaedge.io>
2026-02-12 15:46:05 +0530

    Feat: add alembic migrations

---
+++ alembic/env.py
@@ -0,0 +1,77 @@
+import asyncio
+from logging.config import fileConfig
+
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
+from app.config import DATABASE_URL
+from alembic import context
+
+from app.db.db import Base
+from app.db.models import *  
+
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+from app.config import DATABASE_URL_SYNC
+config = context.config
+
+
+config.set_main_option(
+    "sqlalchemy.url",
+    DATABASE_URL_SYNC.replace("%", "%%")
+)
+
+
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline():
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        compare_type=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: Connection):
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online():
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
+
