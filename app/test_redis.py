2bf8f2f84331be91fdef50032ef80d324f4619a5
aaditya.jaiswal <aaditya.jaiswal@gammaedge.io>
2026-02-12 11:51:27 +0530

    added redis connection testing file

---
+++ app/test_redis.py
@@ -0,0 +1,31 @@
+import asyncio
+import redis.asyncio as redis
+import os
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+REDIS_URL = os.getenv("REDIS_URL")
+
+async def test():
+    try:
+        r = redis.from_url(REDIS_URL, decode_responses=True)
+
+       
+        response = await r.ping()
+        print("Ping response:", response)
+
+       
+        await r.set("test_key", "hello_redis", ex=30)
+
+        
+        value = await r.get("test_key")
+        print("Value from Redis:", value)
+
+        print("Redis connection working!")
+
+    except Exception as e:
+        print(" Redis connection failed:", e)
+
+asyncio.run(test())
