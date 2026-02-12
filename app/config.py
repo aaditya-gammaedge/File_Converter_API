a095f8d74ee82c07b34b8842df5e4afad761a3c7
aaditya.jaiswal <aaditya.jaiswal@gammaedge.io>
2026-02-12 10:49:27 +0530

    Updated: config.py added credentials for supabase and redis

--- app/config.py
+++ app/config.py
@@ -1,39 +1,22 @@
-# import os 
-# from dotenv import load_dotenv
-
-# from pathlib import Path
-
-
-# load_dotenv(".env")   
-
-
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
-# JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-# JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
-# JWT_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES"))
-# SUPABASE_URL = os.getenv("SUPABASE_URL")
-# SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
 import os
 from dotenv import load_dotenv
 
-# Load .env file
+
 load_dotenv(".env")
 
-# Database
+##Database
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# JWT
+#jwt
+
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 JWT_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES", "60"))
 
-# # Supabase
-# SUPABASE_URL = os.getenv("SUPABASE_URL")
-# SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
-# Optional defaults
+
+#file
 MAX_UPLOAD_SIZE_MB = int(os.getenv("MAX_UPLOAD_SIZE_MB", "20"))
 PRESIGNED_URL_EXPIRE_SEC = int(os.getenv("PRESIGNED_URL_EXPIRE_SEC", "300"))
 SUPABASE_S3_ENDPOINT = os.getenv("SUPABASE_S3_ENDPOINT")
@@ -42,3 +25,16 @@ SUPABASE_ACCESS_KEY = os.getenv("SUPABASE_ACCESS_KEY")
     
 SUPABASE_SECRET_KEY =os.getenv("SUPABASE_SECRET_KEY")
 SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET")
+
+
+#Redis
+
+UPSTASH_REDIS_HOST = os.getenv("UPSTASH_REDIS_HOST")
+UPSTASH_REDIS_PORT = os.getenv("UPSTASH_REDIS_PORT")
+UPSTASH_REDIS_PASSWORD = os.getenv("UPSTASH_REDIS_PASSWORD")
+REDIS_URL=os.getenv("REDIS_URL")
+
+
+if not UPSTASH_REDIS_PORT:
+    raise RuntimeError("REDIS_PORT not found in environment variables")
+
