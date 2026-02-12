ca1fee0cb00a9aaf0c2a98d18671d5bcec8ea880
aaditya.jaiswal <aaditya.jaiswal@gammaedge.io>
2026-02-12 10:58:24 +0530

    Feat: added redis client

---
+++ app/worker/redis_client.py
@@ -0,0 +1,24 @@
+import redis
+from app.config import (
+    UPSTASH_REDIS_HOST,
+    UPSTASH_REDIS_PORT,
+    UPSTASH_REDIS_PASSWORD
+)
+
+redis_client = redis.Redis(
+    host=UPSTASH_REDIS_HOST,
+    port=int(UPSTASH_REDIS_PORT),
+    password=UPSTASH_REDIS_PASSWORD,
+    ssl=True,               
+    decode_responses=True
+)
+
+
+def get_next_job():
+    job = redis_client.blpop("convert_queue", timeout=0)
+    if job:
+        return job[1]
+    return None
+
+
+
