1e33142a7b6f46629a6103ba94b347e1743d5f16
aaditya.jaiswal <aaditya.jaiswal@gammaedge.io>
2026-02-12 11:00:31 +0530

    Added worker logic and job processor

---
+++ app/worker/job_processor.py
@@ -0,0 +1,117 @@
+
+import os
+import uuid
+import tempfile
+from sqlalchemy import select
+from app.db.db import AsyncSessionLocal
+from app.db.models.job import Job
+from app.db.models.file import File
+from app.db.models.enums import JobStatusEnum, FileStatusEnum
+from app.services.storage_service import StorageService
+
+from app.worker.converters.pdf_docx_converter import PDFtoDOCXConverter, DOCXtoPDFConverter
+from app.worker.converters.image_converter import JPGtoPNGConverter, PNGtoJPGConverter
+from app.worker.converters.csv_json_excel_converter import CSVtoJSONConverter
+from app.worker.converters.csv_json_excel_converter import CSVtoExcelConverter
+
+
+async def process_job(job_id: str):
+
+    async with AsyncSessionLocal() as db:
+
+        result = await db.execute(
+            select(Job).where(Job.id == job_id)
+        )
+        job = result.scalar_one_or_none()
+
+        if not job:
+            return
+
+        job.status = JobStatusEnum.PROCESSING
+        await db.commit()
+
+        file = await db.get(File, job.file_id)
+
+        try:
+            # ---------- TEMP FILES ----------
+            temp_input = tempfile.NamedTemporaryFile(delete=False)
+            temp_output = tempfile.NamedTemporaryFile(delete=False)
+
+            # ---------- DOWNLOAD ORIGINAL ----------
+            StorageService.download_file(
+                file.storage_path,
+                temp_input.name
+            )
+
+            ext = file.original_filename.split(".")[-1].lower()
+
+            # ---------- CONVERT ----------
+          
+
+            if ext == "pdf":
+                output_ext = "docx"
+                output_path = temp_output.name + ".docx"
+
+                converter = PDFtoDOCXConverter()
+                converter.convert(temp_input.name, output_path)
+
+
+            elif ext == "docx":
+                output_ext = "pdf"
+                output_path = temp_output.name + ".pdf"
+
+                converter = DOCXtoPDFConverter()
+                converter.convert(temp_input.name, output_path)
+
+
+            elif ext in ["jpg", "png", "webp"]:
+                output_ext = "png"
+                output_path = temp_output.name + ".png"
+
+                converter = JPGtoPNGConverter()
+                converter.convert(temp_input.name, output_path)
+
+
+            elif ext == "csv":
+                output_ext = "json"
+                output_path = temp_output.name + ".json"
+
+                converter = CSVtoJSONConverter()
+                converter.convert(temp_input.name, output_path)
+
+
+            elif ext == "csv_excel":   # optional custom case
+                output_ext = "xlsx"
+                output_path = temp_output.name + ".xlsx"
+
+                converter = CSVtoExcelConverter()
+                converter.convert(temp_input.name, output_path)
+
+
+            else:
+                raise Exception("Unsupported file format")
+
+
+            # ---------- UPLOAD RESULT ----------
+            output_key = f"converted/{file.user_id}/{job.id}/output.{output_ext}"
+
+            StorageService.upload_file(
+                temp_output.name,
+                output_key
+            )
+
+            # ---------- UPDATE DB ----------
+            job.status = JobStatusEnum.COMPLETED
+            job.output_storage_path = output_key
+            file.status = FileStatusEnum.COMPLETED
+
+            await db.commit()
+
+            print("Job completed:", job_id)
+
+        except Exception as e:
+            job.status = JobStatusEnum.FAILED
+            file.status = FileStatusEnum.FAILED
+            await db.commit()
+
+            print("Job failed:", str(e))
---
+++ app/worker/worker.py
@@ -0,0 +1,19 @@
+
+import asyncio
+from app.worker.redis_client import get_next_job
+from app.worker.job_processor import process_job
+
+
+async def start_worker():
+    print("Worker started..............")
+
+    while True:
+        job_id = get_next_job()
+
+        if job_id:
+            print("Processing:", job_id)
+            await process_job(job_id)
+
+
+if __name__ == "__main__":
+    asyncio.run(start_worker())
